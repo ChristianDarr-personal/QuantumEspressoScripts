587dcaf66b5a4c486a2a46bb923ca58a6a04c249
Sahuj Mehta <Sahuj.mehta@gmail.com>
2022-06-09 17:30:03 -0500

    Added args and file path

--- script.py
+++ script.py
@@ -1,4 +1,18 @@
 import subprocess
+import sys
+import os
+
+ 
+if len(sys.argv) != 3:
+    raise ValueError('Usage: arg[1] = Compound Name, arg[2] = Structure')
+ 
+compound = sys.argv[1]
+structure = sys.argv[2]
+
+dir = "Results\\"+compound+'_'+structure;
+if not os.path.exists(dir):
+    os.mkdir(dir)
+
 
 
 
